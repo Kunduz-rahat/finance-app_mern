b4b2f9f27db6b3bc87f6ff308c87569404794981
Kunduz Rahat <rahatova.kunduz@gmail.com>
2023-07-18 12:55:45 +0600

    add header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,30 @@
 import { useMemo } from "react";
-import {createTheme} from '@mui/material/styles'
-import {themeSettings} from './theme'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import { themeSettings } from "./theme";
+import { Dashboard } from "./pages/Dashboard.tsx";
+import { Navbar } from "./scenes/navbar/index.tsx";
+
 function App() {
-  const theme = useMemo(()=>createTheme(themeSettings), [])
-  return <div className="app">aff</div>;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding='1rem 2rem 4rem 2rem'>
+            <Navbar/>
+            <Routes>
+              <Route path="/" element={<Dashboard/>}/>
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/scenes/navbar/index.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return <div>navbar</div>;
+};
+
+export default Navbar;
